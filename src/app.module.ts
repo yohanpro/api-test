02247c44fa20e57bb2afaca40bfed8303192aadb
Yohan <yohan@yohanpro.com>
2021-03-30 22:38:44 +0900

    feat: Auth, users services

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { Module } from '@nestjs/common';
 import { MoviesController } from './movies/movies.controller';
 import { UsersController } from './users/users.controller';
 import { MoviesService } from './movies/movies.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UsersModule],
   controllers: [MoviesController, UsersController],
   providers: [MoviesService],
 })
